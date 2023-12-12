1bca4e0cf649a85246bdd74ded55450389cb2474
gitazeinstra <gitazeinstra@gmail.com>
2023-12-12 22:55:42 +0100

    longpress

--- assets/script/main.js
+++ assets/script/main.js
@@ -16,6 +16,31 @@ design.addEventListener('dblclick', function onClick(event) {
   event.target.style.borderColor = 'var(--secondary)';
 });
 
+let and = document.querySelector('a:nth-of-type(3)')
+let delay;
+let longpress = 1300;
+let development = document.querySelector('a:nth-of-type(4)')
+
+development.addEventListener('mousedown', function (event) {
+  delay = setTimeout(check, longpress);
+
+  function check() {
+    event.target.style.backgroundColor = 'var(--highlight-secondary';
+    event.target.style.color = 'var(--secondary)';
+    event.target.style.borderColor = 'var(--secondary)';
+  }
+
+}, true);
+
+development.addEventListener('mouseup', function (event) {
+  clearTimeout(delay);
+});
+
+development.addEventListener('mouseout', function (event) {
+  clearTimeout(delay);
+});
+
+
 
 let interaction = document.querySelector('a:nth-of-type(12)')
 
