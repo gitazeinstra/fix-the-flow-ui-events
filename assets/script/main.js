0dfe4f73969c35cc964755012dbe219946ed54ab
gitazeinstra <gitazeinstra@gmail.com>
2023-12-12 22:01:18 +0100

    color change

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,15 @@
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+
+frontend.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'var(--highlight-secondary';
+  event.target.style.color = 'var(--secondary)';
+  event.target.style.borderColor = 'var(--secondary)';
+});
+
 let interaction = document.querySelector('a:nth-of-type(12)')
 
-interaction.addEventListener('click', jumpHandler)
+interaction.addEventListener('click', jumpHan6dler)
 interaction.addEventListener('animationend', jumpHandler)
 
 function jumpHandler() {
