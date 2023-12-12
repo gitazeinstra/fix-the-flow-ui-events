ba28d7e52c19875c8ca5260bab9861dc82822a39
gitazeinstra <gitazeinstra@gmail.com>
2023-12-12 23:59:39 +0100

    wheel

--- assets/script/main.js
+++ assets/script/main.js
@@ -21,7 +21,6 @@ and.addEventListener('keydown', function() {
   and.classList.toggle('color-change');
 });
 
-
 let development = document.querySelector('a:nth-of-type(4)')
 let delay;
 let longpress = 1300;
@@ -44,7 +43,12 @@ development.addEventListener('mouseout', function () {
   clearTimeout(delay);
 });
 
-
+let sprint = document.querySelector('a:nth-of-type(5)')
+sprint.addEventListener("wheel", function(event) {
+  event.target.style.backgroundColor = 'var(--highlight-secondary';
+  event.target.style.color = 'var(--secondary)';
+  event.target.style.borderColor = 'var(--secondary)';
+});
 
 let interaction = document.querySelector('a:nth-of-type(12)')
 
