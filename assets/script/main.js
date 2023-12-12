742e0308badb39d9f9fc0956dd4266943d5e8f29
gitazeinstra <gitazeinstra@gmail.com>
2023-12-12 22:02:32 +0100

    double click color change

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,6 +7,16 @@ frontend.addEventListener('click', function onClick(event) {
   event.target.style.borderColor = 'var(--secondary)';
 });
 
+let design = document.querySelector('a:nth-of-type(2)')
+
+
+design.addEventListener('dblclick', function onClick(event) {
+  event.target.style.backgroundColor = 'var(--highlight-secondary';
+  event.target.style.color = 'var(--secondary)';
+  event.target.style.borderColor = 'var(--secondary)';
+});
+
+
 let interaction = document.querySelector('a:nth-of-type(12)')
 
 interaction.addEventListener('click', jumpHan6dler)
